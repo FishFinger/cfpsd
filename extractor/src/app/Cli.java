c115576b947eaf0c66433c9cd2f8a7eeee8e9e96
Clément SIPIETER <c.sipieter@gmail.com>
2012-05-26 16:02:31 +0200

    add function searchDate

--- extractor/src/app/Cli.java
+++ extractor/src/app/Cli.java
@@ -1,5 +1,10 @@
 package app;
 
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Cli
 {
 
@@ -8,7 +13,34 @@ public class Cli
    */
   public static void main(String[] args)
   {
-    System.out.println("Hi guy");
+    Scanner scan = new Scanner(System.in);
+    String text = "";
+    
+    while( scan.hasNextLine())
+      {
+        text += scan.nextLine();
+      }
+    
+    System.out.println(searchDate("iest 12/01/2004 siuate"));
+    
+ 
+ 
+  }
+  
+  
+  private static String[] searchDate(String text)
+  {
+    String[] res;
+    
+    Pattern p = Pattern.compile("[0-9]{1,2}/[0-9]{1,2}/[0-9]{2}");
+    Matcher m = p.matcher(text);    
+    MatchResult result = m.toMatchResult();
+    
+    res = new String[result.groupCount()];
+    for (int i=1; i<=result.groupCount(); i++)
+        res[i] = result.group(i);
+    
+    return res;
   }
 
 }
