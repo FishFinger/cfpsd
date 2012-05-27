0738fc867274278429abdbec8d3a87ceb4aee5a5
Thibaut marmin <marminthibaut@gmail.com>
2012-05-28 01:40:47 +0200

    add keyword detection, by enumration

--- extractor/src/app/Cli.java
+++ extractor/src/app/Cli.java
@@ -1,6 +1,5 @@
 package app;
 
-
 import static org.junit.Assert.assertEquals;
 
 import java.text.DateFormatSymbols;
@@ -14,18 +13,18 @@ import java.util.regex.Pattern;
 
 public class Cli
 {
- 
 
   /**
    * @param args
    */
-  /*public static void main(String[] args)
-  {
-
-    System.out
-        .println(searchDate("i 12-12-201423434 12 01 2004 est 12/01/04a si 12-01-2004 ua12te"));
-
-  }*/
+  /* public static void main(String[] args)
+   * {
+   * 
+   * System.out
+   * .println(searchDate(
+   * "i 12-12-201423434 12 01 2004 est 12/01/04a si 12-01-2004 ua12te"));
+   * 
+   * } */
 
   private static String read()
   {
@@ -39,7 +38,7 @@ public class Cli
 
     return text;
   }
-  
+
   public static LinkedList<DateWithPosition> searchDate(String text)
   {
     System.out.println(text);
@@ -58,40 +57,66 @@ public class Cli
 
     return list;
   }
-  
-  
+
+  public static LinkedList<Integer> searchKeyWords(String text)
+  {
+    System.out.println(text);
+    LinkedList<Integer> list = new LinkedList<Integer>();
+
+    Pattern p = KeyWordPattern.getPattern();
+    Matcher matcher = p.matcher(text);
+
+    while (matcher.find())
+      {
+        System.out.println(matcher.group(1));
+        list.add(matcher.start(1));
+      }
+
+    System.out.println();
+
+    return list;
+  }
+
   // ***************************************************************************
   // TEST METHODS
   // ***************************************************************************
 
-  
   public static void main(String args[])
-  {    
-    LinkedList<DateWithPosition> list;
-    
-    list = searchDate("Lundi 3 Janvier 04");
-    System.out.println(list.size());
-    
-    for(DateWithPosition date : list)
+  {
+
+    LinkedList<DateWithPosition> date_list;
+
+    date_list = searchDate("Lundi 3 Janvier 04");
+    System.out.println(date_list.size());
+
+    for (DateWithPosition date : date_list)
       System.out.println(date);
+
+    System.out.println("0/"
+        + Cli.searchDate("15 April 2005").get(0).getPosition());
+    System.out.println("1/"
+        + Cli.searchDate(" 15 April 2005").get(0).getPosition());
+    System.out.println("2/"
+        + Cli.searchDate("  15 April 2005").get(0).getPosition());
+    System.out.println("3/"
+        + Cli.searchDate("   15 April 2005").get(0).getPosition());
+    System.out.println("4/"
+        + Cli.searchDate("    15 April 2005").get(0).getPosition());
+    System.out.println("5/"
+        + Cli.searchDate("     15 April 2005").get(0).getPosition());
+    System.out.println("8/"
+        + Cli.searchDate("Hi guy: 15 April 2005").get(0).getPosition());
+    System.out.println("12/"
+        + Cli.searchDate("1 et 2 et 3 15 April 2005").get(0).getPosition());
+
+    System.out.println("12/"
+        + Cli.searchDate("Salut mec : 15 Avril 2005").get(0).getPosition());
+    System.out.println("12/"
+        + Cli.searchDate("1 et 2 et 3 15 Avril 2005").get(0).getPosition());
     
-    System.out.println("0/" + Cli.searchDate("15 April 2005").get(0).getPosition());
-    System.out.println("1/" + Cli.searchDate(" 15 April 2005").get(0).getPosition());
-    System.out.println("2/" + Cli.searchDate("  15 April 2005").get(0).getPosition());
-    System.out.println("3/" + Cli.searchDate("   15 April 2005").get(0).getPosition());
-    System.out.println("4/" + Cli.searchDate("    15 April 2005").get(0).getPosition());
-    System.out.println("5/" + Cli.searchDate("     15 April 2005").get(0).getPosition());
-    System.out.println("8/" + Cli.searchDate("Hi guy: 15 April 2005").get(0)
-        .getPosition());
-    System.out.println("12/" + Cli.searchDate("1 et 2 et 3 15 April 2005").get(0)
-        .getPosition()); 
-    
-    System.out.println("12/" + Cli.searchDate("Salut mec : 15 Avril 2005").get(0)
-        .getPosition());
-    System.out.println("12/" + Cli.searchDate("1 et 2 et 3 15 Avril 2005").get(0)
-        .getPosition());
-    
-   
+    System.out.println("Test Keywords");
+    Cli.searchKeyWords("Bonjour, voici la date limite de soumission des papiers : ");
+    Cli.searchKeyWords("Can you submit papers before this date ?!");
 
   }
   
---
+++ extractor/src/app/KeyWordPattern.java
@@ -0,0 +1,35 @@
+package app;
+
+import java.util.regex.Pattern;
+
+public class KeyWordPattern
+{
+  private enum keywords
+  {
+    SUBMITTED, SUBMIT, SOUMISSION, SOUMETTRE
+  }
+
+  static Pattern pattern = null;
+
+  public static Pattern getPattern()
+  {
+    if (pattern == null)
+      {
+        String string_pattern = "(";
+        for (int i = 0; i < keywords.values().length; ++i)
+          {
+            if (i > 0)
+              string_pattern += "|";
+
+            string_pattern += keywords.values()[i].toString();
+
+          }
+
+        string_pattern += ")";
+        pattern = Pattern.compile(string_pattern, Pattern.CASE_INSENSITIVE);
+      }
+
+    return pattern;
+  }
+
+}
