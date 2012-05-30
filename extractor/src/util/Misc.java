b8f0e9bfa940f1a99d5c2778a2e6d1ed53c70eb5
Clément SIPIETER <c.sipieter@gmail.com>
2012-05-30 17:04:04 +0200

    remove unused import

--- extractor/src/util/Misc.java
+++ extractor/src/util/Misc.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
